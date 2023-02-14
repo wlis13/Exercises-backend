85021bb18096ac78d117ffbf615961a8cca9b135
wlisses <wlissesfernando285@gmail.com>
2023-02-14 19:01:28 -0300

    feito segundo exercicío. encapsulamento

--- exercicios_backend/POO-and-SOLID/Dia01-Introducao/src/index.js
+++ exercicios_backend/POO-and-SOLID/Dia01-Introducao/src/index.js
@@ -1,14 +1,64 @@
 var TVs = /** @class */ (function () {
-    function TVs(brand, size, resolution, connection) {
+    function TVs(brand, size, resolution, connection, connectedTo) {
         this._brand = brand;
         this._size = size;
         this._resolution = resolution;
         this._connection = connection;
+        this._connectedTo = connectedTo;
     }
     TVs.prototype.turnOn = function () {
         console.log("\n    brand: " + this._brand + ",\n    size: " + this._size + ",\n    resolution: " + this._resolution + ",\n    connection: " + this._connection + ",\n    ");
     };
+    Object.defineProperty(TVs.prototype, "brand", {
+        get: function () {
+            return this._brand;
+        },
+        set: function (newValue) {
+            if (newValue === 'Playstation') {
+                this._brand = newValue;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(TVs.prototype, "size", {
+        get: function () {
+            return this._size;
+        },
+        set: function (newValue) {
+            if (newValue > 0) {
+                this._size = newValue;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    TVs.prototype.newSize = function () {
+        this._size += 1;
+    };
+    Object.defineProperty(TVs.prototype, "connectedTo", {
+        get: function () {
+            return this._connectedTo;
+        },
+        set: function (newValue) {
+            if (this._connection.includes(newValue)) {
+                this._connectedTo = newValue;
+            }
+            else {
+                console.log('Sorry, connection unavailable!');
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     return TVs;
 }());
-var tv01 = new TVs('Sony', 40, '1920X1080', ['HDMI', 'Ethernet', 'VGA']);
+var tv01 = new TVs('Sony', 40, '1920X1080', ['HDMI', 'Ethernet', 'VGA'], 'Ethernet');
 tv01.turnOn();
+tv01.size = 42;
+tv01.brand = 'Playstation';
+tv01.connectedTo = 'HDMI';
+tv01.connectedTo = 'polystation';
+console.log(tv01.size);
+console.log(tv01.brand);
+console.log(tv01.connectedTo);
--- exercicios_backend/POO-and-SOLID/Dia01-Introducao/src/index.ts
+++ exercicios_backend/POO-and-SOLID/Dia01-Introducao/src/index.ts
@@ -1,20 +1,22 @@
 class TVs {
-  _brand: string;
-  _size: number;
-  _resolution: string;
-  _connection: string[];
-  _connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connection: string[];
+  private _connectedTo?: string;
 
   constructor(
     brand: string,
     size: number,
     resolution: string,
-    connection: string[]
+    connection: string[],
+    connectedTo: string
   ) {
     this._brand = brand;
     this._size = size;
     this._resolution = resolution;
     this._connection = connection;
+    this._connectedTo = connectedTo;
   }
 
   turnOn() {
@@ -25,8 +27,56 @@ class TVs {
     connection: ${this._connection},
     `);
   }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newValue: number) {
+    if (newValue > 0) {
+      this._size = newValue;
+    }
+  }
+  newSize() {
+    this._size += 1;
+  }
+  set brand(newValue: string) {
+    if (newValue === 'Playstation') {
+      this._brand = newValue;
+    }
+  }
+
+  set connectedTo(newValue: string) {
+    if (this._connection.includes(newValue)) {
+      this._connectedTo = newValue;
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
+  }
+
+  get connectedTo() {
+    return this._connectedTo as string;
+  }
 }
 
-const tv01 = new TVs('Sony', 40, '1920X1080', ['HDMI', 'Ethernet', 'VGA']);
+const tv01 = new TVs(
+  'Sony',
+  40,
+  '1920X1080',
+  ['HDMI', 'Ethernet', 'VGA'],
+  'Ethernet'
+);
 
 tv01.turnOn();
+tv01.size = 42;
+tv01.brand = 'Playstation';
+tv01.connectedTo = 'HDMI';
+tv01.connectedTo = 'polystation';
+
+console.log(tv01.size);
+console.log(tv01.brand);
+console.log(tv01.connectedTo);
